167bed319d065056ac8464b67c6972c9a8f5f192
Aleix Lafita <aleixlafita@gmail.com>
2019-10-21 15:42:19 +0100

    Include residue names from FASTA sequence into PDB file

--- data_processing/tertiary2pdb.R
+++ data_processing/tertiary2pdb.R
@@ -1,47 +1,90 @@
-# Convert a tertiary prediction from RGN into a PDB file
+# Convert a tertiary prediction from RGN into PDB file format
 # Aleix Lafita - October 2019
 
 library(argparse)
 suppressPackageStartupMessages(library(dplyr))
-#suppressPackageStartupMessages(library(seqinr))
+suppressPackageStartupMessages(library(seqinr))
 
 ###################### Argparse #############################
 
-tertiary = "protein.tertiary"
-fasta = "protein.fa"
-pdb = "protein.pdb"
+tertiary.in = "protein.tertiary"
+fasta.in = "protein.fa"
+pdb.out = "protein.pdb"
 
 # create parser object
 parser = ArgumentParser(
   description='Convert a tertiary prediction from RGN into a PDB file')
 
 # specify our desired options 
-parser$add_argument("-t", "--tertiary", default=tertiary,
+parser$add_argument("-t", "--tertiary", default=tertiary.in,
                     help="Coordinates from RGN for protein structure [default \"%(default)s\"]")
-parser$add_argument("-f", "--fasta", default=fasta,
+parser$add_argument("-f", "--fasta", default=fasta.in,
                     help="Protein sequence in fasta format [default \"%(default)s\"]")
-parser$add_argument("-p", "--pdb", default=pdb,
+parser$add_argument("-p", "--pdb", default=pdb.out,
                     help="Name of the output pdb formatted coordinates [default \"%(default)s\"]")
 
 # get command line options, if help option encountered print help and exit,
 # otherwise if options not found on command line then set defaults, 
 args = parser$parse_args()
 
-tertiary = args$tertiary
-fasta = args$fasta
-pdb = args$pdb
+tertiary.in = args$tertiary
+fasta.in = args$fasta
+pdb.out = args$pdb
 
+aa.codes = c(
+  "A" = "Ala",
+  "C" = "Cys",
+  "D" = "Asp",
+  "E" = "Glu",
+  "F" = "Phe",
+  "G" = "Gly",
+  "H" = "His",
+  "I" = "Ile",
+  "K" = "Lys",
+  "L" = "Leu",
+  "M" = "Met",
+  "N" = "Asn",
+  "P" = "Pro",
+  "Q" = "Gln",
+  "R" = "Arg",
+  "S" = "Ser",
+  "T" = "Thr",
+  "V" = "Val",
+  "W" = "Trp",
+  "Y" = "Tyr"
+) %>% toupper()
+
+############################# File parsing ###############################
+
+# Parse the protein sequence and convert to DF
+seq = read.fasta(fasta.in)
+
+seqlen.fasta = length(getSequence(seq)[[1]])
+
+seq.df = data.frame(
+  pos = seq(1, seqlen.fasta, 1),
+  seq = toupper(unlist(getSequence(seq))),
+  stringsAsFactors = F
+) 
 
 # Parse the tertiary coordinates
 coords = read.csv(
-  tertiary,
+  tertiary.in,
   sep = "",
   comment.char = "#",
   header = F,
   stringsAsFactors = F
 )
 
-seqlen = ncol(coords) / 3
+seqlen.pdb = ncol(coords) / 3
+
+# Stop if the length of the sequence is different than the tertiary
+if(seqlen.fasta != seqlen.pdb) 
+  stop(sprintf("Sequence length in FASTA (%i) different than in tertiary (%i)", seqlen.fasta, seqlen.pdb))
+
+seqlen = seqlen.fasta
+
+############################# Convert ####################################
 
 coords.mat = t(coords)
 
@@ -51,7 +94,6 @@ pdb.df = as.data.frame(coords.mat) %>%
     atomid = seq(1, seqlen*3, 1),
     s1n = 7 - ceiling(log10(atomid + 1)),
     atomn = rep(c("  N   ", "  CA  ", "  C   "), seqlen),
-    resn = "ALA",
     chainid = "A",
     resid = ceiling(atomid / 3),
     s2n = 4 - ceiling(log10(resid + 1)),
@@ -73,7 +115,15 @@ pdb.df = as.data.frame(coords.mat) %>%
     occup = "  1.00",
     bfac = "  0.00",
     atomtype = rep(c("N", "C", "C"), seqlen)
-  ) %>%
+  ) 
+
+# Include the sequence information from FASTA file
+pdb.resn = merge(pdb.df, seq.df, by.x = "resid", by.y = "pos") %>%
+  rowwise() %>%
+  mutate(resn = aa.codes[seq])
+
+# Combine all info into PDB format lines
+pdb.pdbrec = pdb.resn %>%
   rowwise() %>%
   mutate(
     pdbrec = paste0(
@@ -100,9 +150,10 @@ pdb.df = as.data.frame(coords.mat) %>%
     )
   )
 
+# Write the output file
 write.table(
-  pdb.df %>% select(pdbrec),
-  pdb,
+  pdb.pdbrec %>% select(pdbrec),
+  pdb.out,
   row.names = F,
   col.names = F,
   quote = F
